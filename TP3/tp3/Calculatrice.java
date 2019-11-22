110dbaf0a2137c62d48a4bed41d0c2c26226cf87
Autome Edwin <automeedwin@gmail.com>
2019-11-22 10:53:19 +0100

    Déclaration de la méthode carre()

--- TP3/tp3/Calculatrice.java
+++ TP3/tp3/Calculatrice.java
@@ -10,6 +10,14 @@ package tp3;
 public class Calculatrice {
     
     /**
+     * Cette méthode calcule la carré d'un nombre
+     * @param n : un nombre entier quelconque
+     * @return Un nombre >= 0, résultat de la multiplication de n par lui-même
+     */
+    public int carre(int n) {
+	return 0;
+    }
+    /**
      * @param args
      */
     public static void main(String[] args) {
