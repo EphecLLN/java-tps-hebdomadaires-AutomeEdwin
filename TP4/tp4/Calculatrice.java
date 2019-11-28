ae1c4fb9ef81863d2f328a3a216a5ca2dc7f2e89
Autome Edwin <automeedwin@gmail.com>
2019-11-28 10:47:40 +0100

    Ajout de la méthode toString()

--- TP4/tp4/Calculatrice.java
+++ TP4/tp4/Calculatrice.java
@@ -1,65 +1,85 @@
 package tp4;
 
 /**
- * Cette classe modélise une calculatrice simplifiée avec une valeur courante et trois opérations. 
+ * Cette classe modélise une calculatrice simplifiée avec une valeur courante et
+ * trois opérations.
+ * 
  * @author vvandens
  *
  */
 public class Calculatrice {
-	
-	double valeurCourante; // Stocke la valeur affichée sur l'écran de la calculatrice
-	
-	
-	//METHODES
-	public boolean equals(Calculatrice obj) {
-	    return this.valeurCourante == obj.valeurCourante;
+
+    double valeurCourante; // Stocke la valeur affichée sur l'écran de la calculatrice
+
+    // METHODES
+    /**
+     * Indique si 2 objets sont égaux
+     * 
+     * @param obj l'objet à comparer à l'objet courant
+     * @return true si les objets sont égaux
+     */
+    public boolean equals(Calculatrice obj) {
+	return this.valeurCourante == obj.valeurCourante;
+    }
+
+    /**
+     * Indique la valeur courante
+     */
+    public String toString() {
+	return "La valeur courante vaut :" + valeurCourante;
+    }
+
+    /**
+     * Additionne un nombre à la valeur courante
+     * 
+     * @param n le nombre à ajouter à la valeur courante de la calculatrice
+     */
+    void ajoute(double n) {
+	valeurCourante += n;
+    }
+
+    /**
+     * Soustrait un nombre à la valeur courante
+     * 
+     * @param n le nombre à soustraire à la valeur courante de la calculatrice
+     */
+    void soustrait(double n) {
+	valeurCourante -= n;
+    }
+
+    /**
+     * Multiplie la valeur courante par un nombre
+     * 
+     * @param n le multiplicateur
+     */
+    void multi(int n) {
+	valeurCourante *= n;
+    }
+
+    /**
+     * Elève la valeur courante au carré
+     */
+    void carre() {
+	valeurCourante *= valeurCourante;
+    }
+
+    /**
+     * Méthode main permettant de lancer un test rapide de la classe calculatrice.
+     * 
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+	Calculatrice myCalc = new Calculatrice();
+	Calculatrice myCalc2 = new Calculatrice();
+	myCalc.ajoute(5);
+	myCalc2.ajoute(5);
+	myCalc.soustrait(2);
+	myCalc2.soustrait(2);
+
+	if (myCalc.equals(myCalc2)) {
+	    System.out.println("Les deux calculatrices sont égales.");
+	} else {
+	    System.out.println("Les deux calculatrices ne sont pas égales.");
 	}
-	
-	/**
-	 * Additionne un nombre à la valeur courante
-	 * @param n le nombre à ajouter à la valeur courante de la calculatrice
-	 */
-	void ajoute(double n) {
-		valeurCourante+=n;
-	}
-	
-	/**
-	 * Soustrait un nombre à la valeur courante
-	 * @param n le nombre à soustraire à la valeur courante de la calculatrice
-	 */
-	void soustrait(double n) {
-		valeurCourante-=n;
-	}
-	
-	/**
-	 * Multiplie la valeur courante par un nombre
-	 * @param n le multiplicateur
-	 */
-	void multi(int n) {
-	    valeurCourante*=n;
-	}
-	
-	/**
-	 * Elève la valeur courante au carré
-	 */
-	void carre() {
-		valeurCourante*=valeurCourante;
-	}
-	
-	/**
-	 * Méthode main permettant de lancer un test rapide de la classe calculatrice.
-	 * @param args les arguments de la ligne de commande
-	 */
-	public static void main(String[] args) {
-		Calculatrice myCalc = new Calculatrice();
-		Calculatrice myCalc2 = new Calculatrice();
-		myCalc.ajoute(5);
-		myCalc2.ajoute(5);
-		myCalc.soustrait(2);
-		myCalc2.soustrait(2);
-		if(myCalc.equals(myCalc2)) {
-		    
-		}
-		System.out.println(myCalc.valeurCourante);
-	}
+    }
 }
