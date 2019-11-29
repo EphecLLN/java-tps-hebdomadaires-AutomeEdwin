7a0f38d423498e47bf589ac6dfca47e7594cf782
Autome Edwin <automeedwin@gmail.com>
2019-11-29 10:50:40 +0100

    Generation JavaDoc

--- TP1/tp1/package-info.java
+++ TP1/tp1/package-info.java
@@ -1,7 +1,13 @@
 /**
+ * Ce package contient les classes du TP1.
+ *<ul>
+ *<li><code>Calculatrice.java</code></li>
+ *<li><code>Date.java</code></li>
+ *<li><code>Etudiant.java</code></li>
+ *<li><code>HelloWolrd.java</code></li>
+ *</ul> 
  * 
- */
-/**
+ *
  * @author edwin
  *
  */
